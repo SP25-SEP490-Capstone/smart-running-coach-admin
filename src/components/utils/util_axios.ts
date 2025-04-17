552a1a8e87a3d01ca876a17b05e313c648ff7735
WickyPlays <nguyenthienbao8601@gmail.com>
2025-04-17 17:51:24 +0700

    Checkpoint

--- src/components/utils/util_axios.ts
+++ src/components/utils/util_axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./util_cookie";
 
 const MASTER_URL = "http://localhost:5000/api";
-// const MASTER_URL = "http://192.168.1.11:5000/api";
+// const MASTER_URL = "http://192.168.1.10:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: MASTER_URL,
